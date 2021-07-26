89751d6bdc261b26a275af7f8a52dea599418af2
sohiLee <lsohi2468@gmail.com>
2021-07-27 01:31:50 +0900

    todo add clearAll-btn

--- vue-todo/src/components/TodoFooter.vue
+++ vue-todo/src/components/TodoFooter.vue
@@ -1,15 +1,32 @@
 <template>
-  <div>
-      footer
+  <div class="clearAllontainer">
+      <span class="clearAllBtn" v-on:click="clearTodo">Clear All</span>
   </div>
 </template>
 
 <script>
 export default {
+  methods: {
+    clearTodo: function() {
+      localStorage.clear();
+    }
+  }
 
 }
 </script>
 
-<style>
-
+<style scoped>
+.clearAllontainer{
+  width: 8.5rem;
+  height: 50px;
+  line-height:50px;
+  background-color: #fff;
+  border-radius: 5px;
+  margin:0 auto;
+}
+.clearAllBtn{
+  display: block;
+  color:#e20303;
+  cursor: pointer;
+}
 </style>
