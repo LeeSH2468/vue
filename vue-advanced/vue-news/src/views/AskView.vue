48b857cb869a006638f96e497a1ace8887643119
sohiLee <lsohi2468@gmail.com>
2021-08-10 02:12:11 +0900

    mpaGetters

--- vue-advanced/vue-news/src/views/AskView.vue
+++ vue-advanced/vue-news/src/views/AskView.vue
@@ -1,27 +1,28 @@
 <template>
   <div>
-    <div v-for="item in asks"> {{ item.title }} </div>
+    <div v-for="item in fetchedAsk"> {{ item.title }} </div>
   </div>
 </template>
 
 <script>
-import { fetchAskList } from '../api/index'
+import { mapState, mapGetters } from 'vuex';
+
 export default {
-  data() {
-    return {
-      asks: []
-    }    
+  computed: {
+      ...mapGetters([
+         'fetchedAsk'
+      ]),
+    
+    // ...mapState({
+    //   ask: state => state.ask
+    // }),
+
+    // ask(){
+    //   return this.$store.state.asks;
+    // }
   },
   created() {
-    var vm = this;
-    fetchAskList()
-         .then(function(response){
-           console.log(response)
-           vm.asks = response.data;
-         })
-         .catch(function(error) {
-           console.log(error)
-         })
+    this.$store.dispatch('FETCH_ASKS');
   }
 }
 </script>
