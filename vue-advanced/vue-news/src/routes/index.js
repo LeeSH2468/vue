1ce15cf7fe3b3bebc7cc370c33f63aff9e6f7c59
sohiLee <lsohi2468@gmail.com>
2021-08-20 01:19:32 +0900

    use navigation guards

--- vue-advanced/vue-news/src/routes/index.js
+++ vue-advanced/vue-news/src/routes/index.js
@@ -6,6 +6,8 @@ import JobsView from '../views/JobsView.vue';
 import UserView from '../views/UserView.vue';
 import ItemView from '../views/ItemView.vue';
 import createListView from '../views/CreateListView';
+import bus from '../utils/bus';
+import { store } from '../store/index';
 
 Vue.use(VueRouter)
 
@@ -19,20 +21,43 @@ export const router = new VueRouter({
         {
           path:'/news',
           name: 'news',
-          // component: NewsView,
-          component: createListView('NewsView')
+          component: NewsView,
+          beforeEnter: (to, from, next) => {
+            bus.$emit('start:spinner');
+            store.dispatch('FETCH_LIST', to.name)
+                .then(() => {
+                    console.log('fetched');
+                    bus.$emit('end:spinner');
+                    next();
+                })
+                .catch((error) => {
+                    console.log(error);
+                });                 
+          },
+          // component: createListView('NewsView')
         },
         {
           path:'/ask',
           name: 'ask',
-          // component: AskView,
-          component: createListView('AskView')
+          component: AskView,
+          // beforeEnter: (to, from, next) => {
+          //   console.log(to)
+          //   console.log(from)
+          //   console.log(next)                        
+          // },
+          component : TextDecoderStream;,
+          // component: createListView('AskView')
         },
         {
           path:'/jobs',
           name: 'jobs',
-          // component: JobsView,
-          component: createListView('NJobsView')
+          component: JobsView,
+          // beforeEnter: (to, from, next) => {
+          //   console.log(to)
+          //   console.log(from)
+          //   console.log(next)                        
+          // },
+          // component: createListView('NJobsView')
         },
         {
             path:'/user/:id',
