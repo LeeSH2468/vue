315fe963358a515fa2595f49442478a3490e36bb
sohiLee <lsohi2468@gmail.com>
2021-09-08 01:28:01 +0900

    login

---
+++ vue-endgame/vue-til/src/components/LoginForm.vue
@@ -0,0 +1,61 @@
+<template>
+  <form @submit.prevent="submitFrom">
+    <div>
+      <label for="username">ID : </label>
+      <input id="username" type="text" v-model="username" />
+    </div>
+    <div>
+      <label for="password">PW : </label>
+      <input id="password" type="text" v-model="password" />
+    </div>
+    <button :disabled="!isUsernameValid || !password" type="submit">
+      로그인
+    </button>
+    <p>{{ logMessage }}</p>
+  </form>
+</template>
+
+<script>
+import { loginUser } from '@/api/index';
+import { validateEmail } from '@/utils/validation';
+
+export default {
+  data() {
+    return {
+      username: '',
+      password: '',
+      logMessage: '',
+    };
+  },
+  computed: {
+    isUsernameValid() {
+      return validateEmail(this.username);
+    },
+  },
+  methods: {
+    async submitFrom() {
+      try {
+        //   비즈니스로직
+        const userData = {
+          username: this.username,
+          password: this.password,
+        };
+        const { data } = await loginUser(userData);
+        console.log(data.user.username);
+        this.logMessage = `${data.user.username} 님 환영합니다`;
+        this.initForm();
+      } catch (error) {
+        //   에러처리 핸들링
+        console.log(error.response.data);
+        this.logMessage = error.response.data;
+      }
+    },
+    initForm() {
+      this.username = '';
+      this.password = '';
+    },
+  },
+};
+</script>
+
+<style></style>
---
+++ vue-endgame/vue-til/src/utils/validation.js
@@ -0,0 +1,7 @@
+//이메일 유효성 검사(이메일형식)
+function validateEmail(email) {
+  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
+
+export { validateEmail };
