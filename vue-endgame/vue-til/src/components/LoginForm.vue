343680177c79083181c6070dcc7a07e31b0773dd
sohiLee <lsohi2468@gmail.com>
2021-09-13 00:00:00 +0900

    test

--- vue-endgame/vue-til/src/components/LoginForm.vue
+++ vue-endgame/vue-til/src/components/LoginForm.vue
@@ -1,18 +1,31 @@
 <template>
-  <form @submit.prevent="submitFrom">
-    <div>
-      <label for="username">ID : </label>
-      <input id="username" type="text" v-model="username" />
+  <div class="contents">
+    <div class="form-wrapper form-wrapper-sm">
+      <form @submit.prevent="submitForm" class="form">
+        <div>
+          <label for="username">id:</label>
+          <input id="username" type="text" v-model="username" />
+          <p class="validation-text">
+            <span class="warning" v-if="!isUsernameValid && username">
+              Please enter an email address
+            </span>
+          </p>
+        </div>
+        <div>
+          <label for="password">pw:</label>
+          <input id="password" type="text" v-model="password" />
+        </div>
+        <button
+          :disabled="!isUsernameValid || !password"
+          type="submit"
+          class="btn"
+        >
+          로그인
+        </button>
+      </form>
+      <p class="log">{{ logMessage }}</p>
     </div>
-    <div>
-      <label for="password">PW : </label>
-      <input id="password" type="text" v-model="password" />
-    </div>
-    <button :disabled="!isUsernameValid || !password" type="submit">
-      로그인
-    </button>
-    <p>{{ logMessage }}</p>
-  </form>
+  </div>
 </template>
 
 <script>
@@ -22,8 +35,10 @@ import { validateEmail } from '@/utils/validation';
 export default {
   data() {
     return {
+      // form values
       username: '',
       password: '',
+      // log
       logMessage: '',
     };
   },
@@ -33,21 +48,24 @@ export default {
     },
   },
   methods: {
-    async submitFrom() {
+    async submitForm() {
       try {
-        //   비즈니스로직
+        // 비즈니스 로직
         const userData = {
           username: this.username,
           password: this.password,
         };
         const { data } = await loginUser(userData);
-        console.log(data.user.username);
-        this.logMessage = `${data.user.username} 님 환영합니다`;
-        this.initForm();
+        console.log(data.token);
+        this.$store.commit('setToken', data.token);
+        this.$store.commit('setUsername', data.user.username);
+        this.$router.push('/main');
       } catch (error) {
-        //   에러처리 핸들링
+        // 에러 핸들링할 코드
         console.log(error.response.data);
         this.logMessage = error.response.data;
+      } finally {
+        this.initForm();
       }
     },
     initForm() {
@@ -58,4 +76,8 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+.btn {
+  color: white;
+}
+</style>
--- vue-endgame/vue-til/src/views/NotFoundPage.vue
+++ vue-endgame/vue-til/src/views/NotFoundPage.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    페이지를 찾을 수 없습니다.
+    Page is not found
   </div>
 </template>
 
